c7f3e9ff2e3915f60a498a32f76e67cf71960946
TommasoBotarelli <tommaso.botarelli@gmail.com>
2026-07-11 16:15:23 +0200

    Removed unused libraries

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 cd calculus/
 
 mvn install:install-file \
-   -Dfile=libs/eulero-1.0.0-SNAPSHOT.jar \
+   -Dfile=libs/eulero-1.0.0-SNAPSHOT-jar-with-dependencies.jar \
    -DgroupId=org.oris-tool \
    -DartifactId=eulero \
    -Dversion=1.0.0-SNAPSHOT \
